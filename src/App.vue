6763adc9a8cb5c45ccd1d0b733197768fbab7de4
Karl Yang <ccok12345678@gmail.com>
2022-03-18 17:19:31 +0800

    added SideMenu.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template lang="pug">
 .row.g-0
-  .col.col-lg-9.order-1.vh-100
+  .col.col-lg-9.order-1
     router-view
-  aside.col-3.order-0.d-none.d-lg-block.vh-100.sticky-top.bg-success
+  .col-3.order-0.d-none.d-lg-block
+    SideMenu
 </template>
+
+<script>
+import SideMenu from '@/components/SideMenu.vue'
+
+export default {
+  components: {
+    SideMenu
+  }
+}
+</script>
 
 <style lang="scss">
 @import "assets/scss/all.scss";
---
+++ src/components/SideMenu.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+aside.bg-white.min-vh-100.shadow.sticky-top.p-4
+  header.mt-3
+    Logo.mb-4
+    SearchBar
+
+</template>
+
+<script>
+import Logo from '@/components/PageLogo.vue'
+import SearchBar from '@/components/SearchBar.vue'
+
+export default {
+  components: {
+    Logo,
+    SearchBar
+  }
+}
+</script>
