b7b102015501feaab8fd61505ad19a4cfcd57c8f
Karl Yang <ccok12345678@gmail.com>
2022-03-18 15:47:06 +0800

    added NavBar.vue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+nav.bg-white.d-flex.justify-content-center.py-2.ps-3.shadow.d-lg-none
+
+  button.btn.btn-third.border-0.shadow-0.my-auto.me-auto(
+    type="button" title="開啟選單")
+    img(src="@/assets/icons/slider_icon.svg"
+      alt="menu toggle button")
+
+  a.logo.me-auto(href="#" title="Taiwan Travel")
+    img(src="@/assets/images/logo.svg"
+      alt="page logo" height="40")
+</template>
+
+<style lang="scss" scoped>
+$btn-size: 32px;
+
+.btn {
+  position: relative;
+  width: $btn-size;
+  height: $btn-size;
+  vertical-align: middle;
+  padding: 7px 5.5px;
+
+  img {
+    position: absolute;
+    top: 50%; left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    width: 21px;
+  }
+}
+
+.logo {
+  margin-left:  -$btn-size;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
 .min-vh-100.d-flex.flex-column
 
+  NavBar
+
   .container-fluid.p-4
     Banner
 
@@ -10,12 +12,14 @@
 <script>
 import Banner from '@/components/FrontPageBanner.vue'
 import Footer from '@/components/FrontPageFooter.vue'
+import NavBar from '@/components/NavBar.vue'
 
 export default {
   name: 'HomeView',
   components: {
     Banner,
-    Footer
+    Footer,
+    NavBar
   }
 }
 
