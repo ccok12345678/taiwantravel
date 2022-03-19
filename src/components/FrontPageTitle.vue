adc74a67c861befabdf6ba5ccd2aeefe987cdcd6
Karl Yang <ccok12345678@gmail.com>
2022-03-19 17:08:54 +0800

    added FrontPageTitle.vue

---
+++ src/components/FrontPageTitle.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+header.d-flex.justify-content-between.align-items-center
+  .hstack.mb-3
+    img(src="@/assets/icons/location_icon_purple.svg" alt="location icon")
+    h3 {{ header.title }}
+
+  router-link.text-primary.text-no-responsive(:to="`/${header.url}`")
+    | 更多{{ header.title }}
+
+</template>
+
+<script>
+export default {
+  props: {
+    header: {
+      type: Object,
+      default: () => ({
+        title: '探索台灣',
+        url: '/'
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+h3 {
+  margin-left: 12px;
+}
+</style>
--- src/views/FrontPageView.vue
+++ src/views/FrontPageView.vue
@@ -1,3 +1,31 @@
 <template lang="pug">
-h2 here the front page
+section
+  Title(:header="makeHeader('熱門景點', 'attractions')")
+
+section
+  Title(:header="makeHeader('觀光活動', 'activities')")
+
+section
+  Title(:header="makeHeader('美食品嚐', 'tasty')")
+
+section
+  Title(:header="makeHeader('住宿推薦', 'hotels')")
+
 </template>
+
+<script>
+import Title from '@/components/FrontPageTitle.vue'
+
+export default {
+  components: {
+    Title
+  },
+  setup () {
+    const makeHeader = (title, url) => ({ title: title, url: url })
+
+    return {
+      makeHeader
+    }
+  }
+}
+</script>
