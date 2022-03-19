93cb79c42494595026c76c88bb83edaf552ade4a
Karl Yang <ccok12345678@gmail.com>
2022-03-19 14:18:26 +0800

    added CategoryMenu.vue

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,19 +1,28 @@
 <template lang="pug">
 aside.bg-white.min-vh-100.shadow.sticky-top.p-4
-  header.mt-3
+
+  header.side-header.mt-3
     Logo.mb-4
+
     SearchBar
+
+    hr.my-4
+
+  .side-body
+    CategoryMenu
 
 </template>
 
 <script>
 import Logo from '@/components/PageLogo.vue'
 import SearchBar from '@/components/SearchBar.vue'
+import CategoryMenu from '@/components/CategoryMenu.vue'
 
 export default {
   components: {
     Logo,
-    SearchBar
+    SearchBar,
+    CategoryMenu
   }
 }
 </script>
