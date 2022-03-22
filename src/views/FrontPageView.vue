bc9f9ece2f457a8f3a0e281bf0de0c00255a5a91
Karl Yang <ccok12345678@gmail.com>
2022-03-22 22:50:22 +0800

    added VueElementLoading

--- src/views/FrontPageView.vue
+++ src/views/FrontPageView.vue
@@ -9,23 +9,33 @@ Tasty(:tastys="tastys")
 
 Hotels(:hotels="hotels")
 
+VueElementLoading(is-full-screen
+  :active="isLoading"
+  spinner="line-down"
+  size="50"
+  duration="0.8"
+  color="#3FB195"
+  background-color="rgba(50, 50, 50, .8)"
+  text="載入中...")
 </template>
 
 <script>
-import { reactive, onMounted } from 'vue'
+import { reactive, ref } from 'vue'
 import { round } from 'lodash'
 import getData from '@/methods/getData'
 import Attractions from '@/components/FrontPageAttractions.vue'
 import Activities from '@/components/FrontPageActivities.vue'
 import Tasty from '@/components/FrontPageTasty.vue'
 import Hotels from '@/components/FrontPageHotels.vue'
+import VueElementLoading from 'vue-element-loading'
 
 export default {
   components: {
     Attractions,
     Activities,
     Tasty,
-    Hotels
+    Hotels,
+    VueElementLoading
   },
   setup () {
     const attractions = reactive({ list: [] })
@@ -38,51 +48,59 @@ export default {
       lon: ''
     })
 
-    onMounted(() => {
-      try {
-        navigator.geolocation.getCurrentPosition(async (position) => {
-          const latitude = await round(position.coords.latitude, 9)
-          const longitude = await round(position.coords.longitude, 10)
+    const isLoading = ref(false)
 
-          location.lat = latitude
-          location.lon = longitude
-          console.log(location)
+    try {
+      isLoading.value = true
 
-          const attractionApi = `v2/Tourism/ScenicSpot?%24top=3&%24spatialFilter=nearby(${latitude}%2C%20${longitude}%2C%205000)&%24format=JSON`
-          attractions.list = await getData(attractionApi)
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const latitude = await round(position.coords.latitude, 9)
+        const longitude = await round(position.coords.longitude, 10)
 
-          const activitiesApi = `v2/Tourism/Activity?%24top=3&%24spatialFilter=nearby(${latitude}%2C%20${longitude}%2C%2020000)&%24format=JSON`
-          activities.list = await getData(activitiesApi)
+        location.lat = latitude
+        location.lon = longitude
+        console.log(location)
 
-          const tastyApi = `v2/Tourism/Restaurant?%24top=3&%24spatialFilter=nearby(${latitude}%2C%20${longitude}%2C%203000)&%24format=JSON`
-          tastys.list = await getData(tastyApi)
+        const attractionApi = `v2/Tourism/ScenicSpot?%24top=3&%24spatialFilter=nearby(${latitude}%2C%20${longitude}%2C%205000)&%24format=JSON`
+        attractions.list = await getData(attractionApi)
 
-          const hotelsApi = `v2/Tourism/Hotel?%24top=3&%24spatialFilter=nearby(${latitude}%2C%20${longitude}%2C%205000)&%24format=JSON`
-          hotels.list = await getData(hotelsApi)
-        }, async () => {
-          const attractionApi = 'v2/Tourism/ScenicSpot?%24top=3&%24format=JSON'
-          attractions.list = await getData(attractionApi)
+        const activitiesApi = `v2/Tourism/Activity?%24top=3&%24spatialFilter=nearby(${latitude}%2C%20${longitude}%2C%2020000)&%24format=JSON`
+        activities.list = await getData(activitiesApi)
 
-          const activitiesApi = 'v2/Tourism/Activity?%24top=3&%24format=JSON'
-          activities.list = await getData(activitiesApi)
+        const tastyApi = `v2/Tourism/Restaurant?%24top=3&%24spatialFilter=nearby(${latitude}%2C%20${longitude}%2C%203000)&%24format=JSON`
+        tastys.list = await getData(tastyApi)
 
-          const tastyApi = 'v2/Tourism/Restaurant?%24top=3&%24format=JSON'
-          tastys.list = await getData(tastyApi)
+        const hotelsApi = `v2/Tourism/Hotel?%24top=3&%24spatialFilter=nearby(${latitude}%2C%20${longitude}%2C%205000)&%24format=JSON`
+        hotels.list = await getData(hotelsApi)
 
-          const hotelsApi = 'v2/Tourism/Hotel?%24top=3&%24format=JSON'
-          hotels.list = await getData(hotelsApi)
-        })
-      } catch (error) {
-        console.log('location error:', error)
-      }
-    })
+        isLoading.value = false
+      }, async () => {
+        const attractionApi = 'v2/Tourism/ScenicSpot?%24top=3&%24format=JSON'
+        attractions.list = await getData(attractionApi)
+
+        const activitiesApi = 'v2/Tourism/Activity?%24top=3&%24format=JSON'
+        activities.list = await getData(activitiesApi)
+
+        const tastyApi = 'v2/Tourism/Restaurant?%24top=3&%24format=JSON'
+        tastys.list = await getData(tastyApi)
+
+        const hotelsApi = 'v2/Tourism/Hotel?%24top=3&%24format=JSON'
+        hotels.list = await getData(hotelsApi)
+
+        isLoading.value = false
+      })
+    } catch (error) {
+      isLoading.value = false
+      console.log('location error:', error)
+    }
 
     return {
       attractions,
       activities,
       tastys,
       hotels,
-      location
+      location,
+      isLoading
     }
   }
 }
