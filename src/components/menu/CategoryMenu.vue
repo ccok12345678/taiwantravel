2bcfd57b69114ae68ff2ffc007aaf5aaaa0c23f7
Karl Yang <ccok12345678@gmail.com>
2022-03-27 20:44:31 +0800

    added theme search

--- src/components/menu/CategoryMenu.vue
+++ src/components/menu/CategoryMenu.vue
@@ -20,43 +20,43 @@ export default {
       {
         title: '歷史文化',
         src: 'https://i.ibb.co/Y3HKwsR/Frame-40.png',
-        category: 'Tourism',
-        keywords: ['文化類', '古蹟類']
+        routeName: 'attractionsSearch',
+        keywords: ['文化', '古蹟']
       }, {
         title: '戶外踏青',
         src: 'https://i.ibb.co/qJQLLGd/Frame-40-1.png',
-        category: 'Tourism',
-        keywords: ['都會公園類', '生態類', '林場類', '森林遊樂區類']
+        routeName: 'attractionsSearch',
+        keywords: ['公園類', '生態', '林場', '森林遊樂區']
       }, {
         title: '宗教巡禮',
         src: 'https://i.ibb.co/hCrpLDH/Frame-40-2.png',
-        category: 'Tourism',
-        keywords: ['廟宇類']
+        routeName: 'attractionsSearch',
+        keywords: ['廟', '寺', '教堂', '教會', '壇']
       }, {
         title: '親子活動',
         src: 'https://i.ibb.co/pyXVpSy/Frame-40-3.png',
-        category: 'Activity',
-        keywords: []
+        routeName: 'activitiesSearch',
+        keywords: ['親子']
       }, {
         title: '風景區',
         src: 'https://i.ibb.co/vPDSXd8/Frame-40-4.png',
-        category: 'Tourism',
-        keywords: ['國家風景區類', '自然風景類']
+        routeName: 'attractionsSearch',
+        keywords: ['國家風景區', '自然風景']
       }, {
         title: '美食品嚐',
         src: 'https://i.ibb.co/CVbFpJY/Frame-40-5.png',
-        category: 'Restaurant',
-        keywords: ['小吃/特產類']
+        routeName: 'attractionsSearch',
+        keywords: ['小吃/特產類', '夜市', '老街', '市集', '市場']
       }, {
         title: '住宿推薦',
         src: 'https://i.ibb.co/Wz6kDCt/Frame-40-6.png',
-        category: 'Hotel',
+        routeName: 'hotelsSearch',
         keywords: []
       }, {
         title: '觀光活動',
         src: 'https://i.ibb.co/ncmxHdQ/Frame-40-7.png',
-        category: 'Activity',
-        keywords: []
+        routeName: 'activitiesSearch',
+        keywords: ['觀光']
       }
     ])
 
