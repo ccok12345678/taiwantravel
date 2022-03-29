47aa390ff92f1f18d8384386baafb9933c387bc3
Karl Yang <ccok12345678@gmail.com>
2022-03-29 15:08:15 +0800

    added vue-meta

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,17 @@
     router-view
   .col-3.order-0.d-none.d-lg-block
     SideMenu
+
+metainfo
+  template(v-slot:title='{ metainfo }')
+    | {{ metainfo.titleTemplate(metainfo.title) }}
 </template>
 
 <script>
 import { watch } from 'vue'
 import { useRoute } from 'vue-router'
 import SideMenu from '@/components/menu/SideMenu.vue'
+import { useMeta } from 'vue-meta'
 
 export default {
   components: {
@@ -19,6 +24,15 @@ export default {
     const route = useRoute()
     watch(() => route.path, () => {
       window.scrollTo(0, 0)
+    })
+
+    useMeta({
+      title: '',
+      titleTemplate: (chunk) => {
+        return chunk
+          ? `${chunk} | 台灣旅遊景點導覽 THE F2E`
+          : '台灣旅遊景點導覽 THE F2E'
+      }
     })
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createMetaManager, plugin as metaPlugin } from 'vue-meta'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
@@ -9,9 +10,15 @@ import 'vue3-openlayers/dist/vue3-openlayers.css'
 import VueLoading from '@/components/VueLoading'
 import NoResultMessage from '@/components/NoResultMessage'
 
-createApp(App)
+const metaManager = createMetaManager()
+
+const app = createApp(App)
+
+app
   .use(router)
   .use(OpenLayersMap)
+  .use(metaManager)
+  .use(metaPlugin)
   .component('VueLoading', VueLoading)
   .component('NoResultMessage', NoResultMessage)
   .mount('#app')
